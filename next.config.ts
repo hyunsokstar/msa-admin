f01af58692ab882be0dd9f2d0a5d8ef167a706b2
ohhyunsok <terecal@daum.net>
2024-12-23 10:42:30 +0900

    update3

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
-// // msa-admin\next.config.ts
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
-
+// msa-admin\next.config.ts
 import type { NextConfig } from "next";
-import fs from 'fs';
-import path from 'path';
 
 const nextConfig: NextConfig = {
-  webServer: {  // server가 아닌 webServer 사용
-    https: {
-      key: fs.readFileSync(path.join(process.cwd(), 'ssl', 'private.key')),
-      cert: fs.readFileSync(path.join(process.cwd(), 'ssl', 'certificate.crt'))
-    }
-  }
+  /* config options here */
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+// import fs from 'fs';
+// import path from 'path';
+
+// const nextConfig: NextConfig = {
+//   webServer: {  // server가 아닌 webServer 사용
+//     https: {
+//       key: fs.readFileSync(path.join(process.cwd(), 'ssl', 'private.key')),
+//       cert: fs.readFileSync(path.join(process.cwd(), 'ssl', 'certificate.crt'))
+//     }
+//   }
+// };
+
+// export default nextConfig;
