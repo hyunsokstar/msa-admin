1dedbecf103205db2f012a560c86736465367ee1
ohhyunsok <terecal@daum.net>
2025-01-09 16:56:45 +0900

    update for chat room3

--- src/components/providers/ClientProvider.tsx
+++ src/components/providers/ClientProvider.tsx
@@ -1,34 +1,11 @@
-// src/components/providers/ClientProvider.tsx
-'use client';
+import React from 'react'
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+type Props = {}
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000,
-    },
-  },
-});
-
-export default function ClientProvider({ children }: { children: React.ReactNode }) {
+const ClientProvider = (props: Props) => {
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-      <ToastContainer
-        position="top-center"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  );
+    <div>ClientProvider</div>
+  )
 }
+
+export default ClientProvider
