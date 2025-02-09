c75b6510f565d44c811d2b494967c69017872c60
ohhyunsok <terecal@daum.net>
2025-02-10 07:41:46 +0900

    update for task history

---
+++ src/app/task-admin/wbs-board/task-history.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const TaskHistory = (props: Props) => {
+    return (
+        <div>
+            TaskHistory
+        </div>
+    )
+}
+
+export default TaskHistory
