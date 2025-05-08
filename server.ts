2cf8aafb6c6c5505a46450f4fc27f3f8ff26279f
ohhyunsok <terecal@daum.net>
2025-05-08 22:08:15 +0900

    update for https

--- server.ts
+++ server.ts
@@ -3,15 +3,14 @@ import { createServer } from 'https';
 import { parse } from 'url';
 import next from 'next';
 import fs from 'fs';
-import path from 'path';
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
 const httpsOptions = {
-  key: fs.readFileSync(path.join(__dirname, 'ssl', 'private.key')),
-  cert: fs.readFileSync(path.join(__dirname, 'ssl', 'certificate.crt')),
+  key: fs.readFileSync('/etc/letsencrypt/live/nexus-task-master.shop/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/nexus-task-master.shop/fullchain.pem'),
 };
 
 app.prepare().then(() => {
@@ -19,6 +18,12 @@ app.prepare().then(() => {
     const parsedUrl = parse(req.url!, true);
     handle(req, res, parsedUrl);
   }).listen(443, () => {
-    console.log('> Ready on https://localhost:443');
+    console.log('> Ready on https://nexus-task-master.shop');
   });
+
+  // HTTP to HTTPS 리다이렉션 추가 (선택 사항)
+  require('http').createServer((req, res) => {
+    res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+    res.end();
+  }).listen(80);
 });
